fcb9d8c49e89a510bd74c2bcfa2cf71eadcbe597
Tobias Naftali <tobias.naftali@gmail.com>
2017-03-21 17:04:27 -0300

    Migrated to Python 3.6

--- diagram/Output.py
+++ diagram/Output.py
@@ -6,14 +6,15 @@ def save_diagram(array, matrix):
     # dot = Digraph(comment='Hasse Diagram for divisibility relation of ' + str(number))
     for i in range(len(array)):
         # dot.node(str(array[i]), str(array[i]))
-        line = str(array[i]) + ';'
+        line = str(int(array[i])) + ';'
         for j in range(len(matrix[i])):
             if matrix[i][j] == 1:
                 # dot.edge(str(array[i]), str(array[j]))
                 if i + 1 < len(array) and array[j] != array[i]:
-                    line += str(array[j])
+                    line += str(int(array[j]))
                     if j + 1 < len(matrix[i]):
                         line += ','
+        print(line)
         file.write(line)
         file.write('\n')
     file.close()
